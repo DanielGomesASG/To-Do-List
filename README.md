04e5b340682ca2da18d021a221e032dad23f86c5
Daniel Gomes <danielgomesasg@gmail.com>
2023-10-18 03:30:46 -0300

    To-Do List

---
+++ README.md
@@ -0,0 +1,2 @@
+# To-Do List
+O projeto foi feito em TypeScript, utilizando o padrão MVC, com foco na estruturação em POO. Basicamente, a aplicação segue o padrão MVC, recebendo os dados da tarefa, ou lembrete que o usuário insere através do controller, tratando-os com base no model e por fim, renderizando o resultado pelo view.
---
+++ public/dist/index.js
@@ -0,0 +1,153 @@
+"use strict";
+(function () {
+    // MODEL
+    var NotificationPlatform;
+    (function (NotificationPlatform) {
+        NotificationPlatform["SMS"] = "SMS";
+        NotificationPlatform["EMAIL"] = "EMAIL";
+        NotificationPlatform["PUSH_NOTIFICATION"] = "PUSH_NOTIFICATION";
+    })(NotificationPlatform || (NotificationPlatform = {}));
+    var ViewMode;
+    (function (ViewMode) {
+        ViewMode["TODO"] = "TODO";
+        ViewMode["REMINDER"] = "REMINDER";
+    })(ViewMode || (ViewMode = {}));
+    var DateUtils = {
+        tomorrow: function () {
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
+        },
+        today: function () {
+            return new Date();
+        },
+        formatDate: function (date) {
+            return "".concat(date.getDate(), ".").concat(date.getMonth() + 1, ".").concat(date.getFullYear());
+        },
+    };
+    var NotificationUtils = {
+        formatNotification: function (notification) {
+            var formattedNotification = notification.join(', ');
+            var formattedString = formattedNotification.replace(/[^a-zA-Z0-9\s]/g, ' ').toLowerCase();
+            var formattedWords = formattedString.split(' ').map(function (word) { return word.charAt(0).toUpperCase() + word.slice(1); });
+            return formattedWords.join(' ');
+        }
+    };
+    var UUID = function () {
+        return Math.random().toString(32).substring(2, 9);
+    };
+    var Reminder = /** @class */ (function () {
+        function Reminder(description, date, notifications) {
+            this.id = UUID();
+            this.dateCreated = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
+            this.description = '';
+            this.date = DateUtils.tomorrow();
+            this.notifications = [NotificationPlatform.EMAIL];
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+        ;
+        Reminder.prototype.render = function () {
+            return "\n      ---> Reminder <---\n      Description: ".concat(this.description, "\n      Date: ").concat(DateUtils.formatDate(this.date), "\n      Platform: ").concat(NotificationUtils.formatNotification(this.notifications), "\n      ");
+        };
+        ;
+        return Reminder;
+    }());
+    var Todo = /** @class */ (function () {
+        function Todo(description) {
+            this.id = UUID();
+            this.dateCreated = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
+            this.description = '';
+            this.done = false;
+            this.description = description;
+        }
+        ;
+        Todo.prototype.render = function () {
+            return "\n      ---> TODO <---\n      Description: ".concat(this.description, "\n      Done: ").concat(this.done, "\n      ");
+        };
+        ;
+        return Todo;
+    }());
+    // Objetos Teste
+    // const todo = new Todo('Todo criado com a classe');
+    // const reminder = new Reminder('Reminder criado com a classe', new Date(), [NotificationPlatform.EMAIL]);
+    //___________________________________________________________________________________________________________
+    // VIEW
+    var taskView = {
+        getTodo: function (form) {
+            var todoDescription = form.todoDescription.value;
+            form.reset();
+            return new Todo(todoDescription);
+        },
+        getReminder: function (form) {
+            var reminderNotifications = [form.notification.value];
+            var reminderDate = new Date(form.reminderDate.value);
+            var reminderDescription = form.reminderDescription.value;
+            form.reset();
+            return new Reminder(reminderDescription, reminderDate, reminderNotifications);
+        },
+        render: function (tasks, mode) {
+            var tasksList = document.getElementById('tasksList');
+            while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement('LI');
+                var textNode = document.createTextNode(task.render());
+                li.appendChild(textNode);
+                tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
+            });
+            var todoSet = document.getElementById('todoSet');
+            var reminderSet = document.getElementById('reminderSet');
+            if (mode === ViewMode.TODO) {
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute('style', 'display: block');
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.removeAttribute('disabled');
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute('style', 'display: none');
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute('disabled', 'true');
+            }
+            else {
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute('style', 'display: block');
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.removeAttribute('disabled');
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute('style', 'display: none');
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute('disabled', 'true');
+            }
+        },
+    };
+    //___________________________________________________________________________________________________________
+    // CONTROLLER
+    var TaskController = function (view) {
+        var _a, _b;
+        var tasks = [];
+        var mode = ViewMode.TODO;
+        var handleEvent = function (event) {
+            event.preventDefault();
+            var form = event.target;
+            switch (mode) {
+                case ViewMode.TODO:
+                    tasks.push(view.getTodo(form));
+                    break;
+                case ViewMode.REMINDER:
+                    tasks.push(view.getReminder(form));
+                    break;
+            }
+            view.render(tasks, mode);
+        };
+        var handleToggleMode = function () {
+            switch (mode) {
+                case ViewMode.TODO:
+                    mode = ViewMode.REMINDER;
+                    break;
+                case ViewMode.REMINDER:
+                    mode = ViewMode.TODO;
+                    break;
+            }
+            view.render(tasks, mode);
+        };
+        (_a = document.getElementById('toggleMode')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', handleToggleMode);
+        (_b = document.getElementById('taskForm')) === null || _b === void 0 ? void 0 : _b.addEventListener('submit', handleEvent);
+    };
+    TaskController(taskView);
+})();
